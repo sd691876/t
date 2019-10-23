1a18b9455f89714a917f0a7598ae2b5d634f0e7a
sd691876 <sd691876@gmail.com>
2019-10-23 22:50:17 +0800

    test

---
+++ KL_HW2/app/src/main/java/com/example/kl_hw2/Main2Activity.kt
@@ -0,0 +1,58 @@
+package com.example.kl_hw2
+
+import android.R
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
+
+import androidx.appcompat.app.AppCompatActivity
+
+class Main2Activity : AppCompatActivity() {
+    private var send_btn: Button? = null
+    private var set_drink: EditText? = null
+    private var rg1: RadioGroup? = null
+    private var rg2: RadioGroup? = null
+    private var sugar = "微糖"
+    private var ice_opt = "微冰"
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        rg1 = findViewById(R.id.radioGroup) as RadioGroup
+        rg1!!.setOnCheckedChangeListener { radioGroup, i ->
+            when (i) {
+                R.id.radioButton1 -> sugar = "無糖"
+                R.id.radioButton2 -> sugar = "少糖"
+                R.id.radioButton3 -> sugar = "半糖"
+                R.id.radioButton4 -> sugar = "全糖"
+            }
+        }
+        rg2 = findViewById(R.id.radioGroup2) as RadioGroup
+        rg2!!.setOnCheckedChangeListener { radioGroup, i ->
+            when (i) {
+                R.id.radioButton5 -> ice_opt = "微冰"
+                R.id.radioButton6 -> ice_opt = "少冰"
+                R.id.radioButton7 -> ice_opt = "正常冰"
+            }
+        }
+        send_btn = findViewById(R.id.btn_send)
+        send_btn!!.setOnClickListener {
+            set_drink = findViewById(R.id.ed_drink)
+            val drink = set_drink!!.text.toString()
+            val i = Intent()
+            val b = Bundle()
+            b.putString("sugar", sugar)
+            b.putString("drink", drink)
+            b.putString("ice", ice_opt)
+            i.putExtras(b)
+            setResult(101, i)
+            finish()
+        }
+    }
+}
+
